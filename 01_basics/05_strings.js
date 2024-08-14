046678d52f99af1e347b21023fa771e0f922b1d0
AKSHAT SONI <121639424+1Aksoni@users.noreply.github.com>
2024-08-14 18:31:24 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,10 @@
+const name ="Akshat"
+const repoCount =50
+
+// console.log(name + repocount +"Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("AkshatSoni");
+
+console.log(gameName);
