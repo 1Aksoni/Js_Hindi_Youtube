91d50aea10c99b53a78233be0bdb569583392c1c
AKSHAT SONI <121639424+1Aksoni@users.noreply.github.com>
2024-08-14 13:09:37 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId =144553;
+let accountEmail ="ak@gmail.com";
+var accountPassword= "12345";
+accountCity ="Jaipur";
+let accountState;
+// accountId =2;
+accountEmail="gg@gmal.com";
+accountPassword="lll";
+accountCity="Indore";   
+
+// Prefer not to use var 
+// because of issue in block scope and functional scope
+
+
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //We are using nodejs , not browser
+
+let name="Akshat"
+let age =23
+let isLoggedIn =false
+
+//number => 2 to power 53
+// bigint
+//string =""
+//boolean=> true/false
+//null => standalone value
+//Undefined => value is not assign
+//symbol => To find uniqueness
+//object
+
+console.log(typeof undefined)  // Type of undefined is undefined
+console.log(typeof null) // Type of null is object
+
