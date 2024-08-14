f5a2cd80dce01d3d15eb42a45f3ab3f915a5a197
AKSHAT SONI <121639424+1Aksoni@users.noreply.github.com>
2024-08-14 13:36:11 +0000

    conversion Operations

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,30 @@
+
+let score ="33abc" 
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) 
+
+// conversion
+
+// "33" => 33
+// "33abc"=> NaN  (Not a Number) and typeof(NaN) is a Number
+// "True" => 1; "False"=> 0
+
+let isLoggedIn = "Akshat"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 =>true; 0 => false
+// "" => false ;"Akshat" => true
+ 
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof(stringNumber))
