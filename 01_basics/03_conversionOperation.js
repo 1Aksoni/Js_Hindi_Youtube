3cd10062838f463120e6656e68cc9076343b833e
AKSHAT SONI <121639424+1Aksoni@users.noreply.github.com>
2024-08-14 14:09:05 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,52 @@
+
+let score ="33abc" 
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) 
+
+// conversion
+
+// "33" => 33
+// "33abc"=> NaN  (Not a Number) and typeof(NaN) is a Number
+// "True" => 1; "False"=> 0
+
+let isLoggedIn = "Akshat"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 =>true; 0 => false
+// "" => false ;"Akshat" => true
+ 
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
+// ********************* Operations *****************************
+
+let value =3
+
+let negValue = -value 
+
+// console.log(negValue)
+
+let str1 = "Akshat"
+let str2 = " Soni"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(typeof("1" + 2))
+// console.log(1 + "2")
+// console.log("1" +2 +2)
+// console.log(1 +2 +"2")
+
+
+
+
