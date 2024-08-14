6ce885266ab9b3760a177d18b32415f2d751543c
AKSHAT SONI <121639424+1Aksoni@users.noreply.github.com>
2024-08-14 18:59:55 +0000

    dates

---
+++ 01_basics/06_date.js
@@ -0,0 +1,15 @@
+let myDate = new Date();
+
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleTimeString());
+// console.log(typeof myDate);
+
+let myNewDate = new Date(2024,0,24,5,3);
+// console.log(myNewDate.toLocaleDateString());
+
+let myTimeStamp = Date.now();
+
+// console.log(myNewDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
